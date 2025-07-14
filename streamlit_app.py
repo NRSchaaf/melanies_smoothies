9f4bbed90ca36d0ae237e8c288eb9d8c2eb9c370
Nathan Schaaf <108774884+NRSchaaf@users.noreply.github.com>
2025-07-14 14:14:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f"Customize Your SMoothie :cup_with_straw:") #{st.__version__}
+st.title(f"My Parents New Healthy Diner:") #{st.__version__}
 st.write(
   """**Choose the fruits you want in your custom Smoothie!**"""
 )
