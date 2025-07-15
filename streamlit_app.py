6142abf1db6649d9c1faa61d1828bfcd310e0b15
Nathan Schaaf <108774884+NRSchaaf@users.noreply.github.com>
2025-07-15 11:13:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Display smoothiefroot nutrition information
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Connect to Snowflake
 cnx = st.connection("snowflake")
